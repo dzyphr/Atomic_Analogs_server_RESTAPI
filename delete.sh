df2b9b34f0197d9b0d4badde1e12401b256672bc
justaguy12345 <ryanberk135@gmail.com>
2024-07-03 12:33:27 -0500

    remove old test  sh files

--- delete.sh
+++
@@ -1,7 +0,0 @@
-curl --request DELETE 'localhost:3030/v0.0.1/requests' \
---header 'Content-Type: application/json' \
---header 'Content-Type: text/plain' \
---data-raw '{
-        "id": "apple"
-}' \
--H "Authorization: Bearer PASSWORD"
--- put.sh
+++
@@ -1,9 +0,0 @@
-curl --location --request PUT 'localhost:3030/v0.0.1/requests' \
---header 'Content-Type: application/json' \
---header 'Content-Type: text/plain' \
---data-raw '{
-        "id": "apple",
-        "request_type": "put"
-}' \
--H "Authorization: Bearer PASSWORD"
-
